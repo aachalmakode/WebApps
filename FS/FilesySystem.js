49f156a9f72f76d2069210064f0c3ae114539b76
Aachal <aachalmakode900@gmail.com>
2021-09-25 16:15:57 -0400

    Added: FileSystem JSON

---
+++ FS/FilesySystem.js
@@ -0,0 +1,20 @@
+const fs = require('fs')
+
+
+//write json file
+fs.writeFile('file.json',JSON.stringify([" Name: Aachal","Last Name: Makode"] ), (err) => {
+if(err) throw err 
+})
+
+//read json file 
+fs.readFile('file.json', 'utf8'
+,(err,content) => {
+    if(err) throw err
+   console.log(content)
+})
+
+//update a json file
+fs.appendFile('file.json', JSON.stringify(["City: Amravati"]),(err) => {
+if(err) throw err
+
+})
